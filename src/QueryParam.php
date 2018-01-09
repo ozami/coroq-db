e3e83d6953c4a83235785bcc24a70603513eb17e
Ozawa Masayuki <ozawa@coroq.com>
2018-01-09 14:47:03 +0900

    Declear properties

--- src/QueryParam.php
+++ src/QueryParam.php
@@ -3,6 +3,16 @@ namespace Coroq\Db;
 
 class QueryParam
 {
+  /** @var mixed */
+  public $value;
+
+  /** @var int */
+  public $type;
+
+  /**
+   * @param mixed $value
+   * @param int $type
+   */
   public function __construct($value, $type = \PDO::PARAM_STR)
   {
     $this->value = $value;
